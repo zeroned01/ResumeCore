3d2fcb2871ba31caa4e00177c304171104e39c2e
Jayesh Vijay <jayeshvijay.jv@gmail.com>
2022-09-10 12:40:57 +0530

    create class

--- Resume/accounts/views.py
+++ Resume/accounts/views.py
@@ -7,12 +7,15 @@ from .forms import SignUpFrom
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 @login_required
 def login_home(request):
     return render(request, 'main.html')
+
 
 def login(request):
     return render(request, 'login.html')
@@ -32,14 +35,18 @@ def signup(request):
 def TermsAndCondition(request):
     return render(request, 'termsAndCondition.html')
 
+
 def password_change(request):
     return render(request, 'password_change_form.html')
+
 
 def aboutUs(request):
     return render(request, 'aboutUs.html')
 
+
 def ContactUs(request):
     return render(request, 'ContactUs.html')
+
 
 def User_input(request):
     return render(request, 'User_input.html')
